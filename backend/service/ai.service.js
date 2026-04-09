18a82269c230c7496696a42ddceca7808acadd87
rudra-netizen <rudrakshtiwari972@gmail.com>
2026-04-09 09:25:23 +0530

    Modifying ReadMe

--- backend/service/ai.service.js
+++ backend/service/ai.service.js
@@ -5,11 +5,11 @@ const ai = new GoogleGenAI({});
 
 async function generateResponse(history) {
   const response = await ai.models.generateContent({
-    model: "gemini-3-flash-preview",
+    model: "gemini-2.5-flash",
     contents: history,
     config: {
       systemInstruction:
-        "You are a profesional replier. use emojis and reply in less words with solid crisp words which leads an impression on the user",
+        "You are a profesional replier. use emojis and reply in less words with solid crisp words which leads an impression on the user. when it comes out as a coding/development area, you have to guide thoroughly to the user. you have to be very helpful and friendly. you have to be very precise and to the point. you have to be very creative and innovative in your responses. you have to be very engaging and interactive in your responses. you have to be very informative and educational in your responses. you have to be very entertaining and fun in your responses. you have to be very persuasive and convincing in your responses. you have to be very inspiring and motivating in your responses.",
     },
   });
   return response.text;
